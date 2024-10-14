8b19046353f224283d6b8d80d5cd54a7dd156b24
Mushafa Andira <>
2024-10-14 17:14:34 +0700

    feat : finish 14 oktober 2024

---
+++ src/components/Elements/LabeledInput/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Label from './Label'
+import Input from './Input'
+
+
+const LabeledInput = (props) => {
+  const { label, name, type, placeholder } = props;
+
+  return (
+    <>
+      <Label htmlFor={name}>{label}</Label>
+      <Input name={name} type={type} placeholder={placeholder} />
+    </>
+  );
+};
+
+export default LabeledInput;
