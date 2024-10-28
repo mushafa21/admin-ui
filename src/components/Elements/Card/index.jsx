43f0da49c3e826edcbbb5fcdffd74ab2cc8cf7d3
Mushafa Andira <>
2024-10-28 15:25:26 +0700

    feat : minggu ke 6

---
+++ src/components/Elements/Card/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <>
+      <div className="text-lg text-gray-02 mb-2">Title1</div>
+      <div className="bg-white rounded-lg px-6 py-5 shadow-xl">widget 1</div>
+    </>
+  );
+};
+
+export default Card;
--- src/components/Elements/Logo/index.jsx
+++ src/components/Elements/Logo/index.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+const Logo = (props) => {
+  const { variant = "text-primary text-4xl" } = props;
 
-const Logo = () => {
   return (
-    <div className="flex justify-center font-poppins tracking-wide text-primary text-4xl">
-    <span className="font-bold ">FINE</span>bank
-    <span className="font-bold">.IO</span>
-  </div>
-    )
-}
+    <div
+      className={`flex justify-center font-poppins tracking-wide ${variant}`}
+    >
+      <span className="font-bold ">FINE</span>bank
+      <span className="font-bold">.IO</span>
+    </div>
+  );
+};
 
-export default Logo
+export default Logo;
